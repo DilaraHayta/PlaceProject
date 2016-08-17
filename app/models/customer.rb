c6c5d2ad8f532698f0c0bf982511f47b23b6bb4d
DilaraHayta <1306130091@ogr.iu.edu.tr>
2016-08-17 16:14:19 +0300

    Seed.rb fixed.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,6 +4,15 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :places, dependent: :destroy
+  has_many :places
   has_many :comments, dependent: :destroy
+
+  before_destroy :assign_to_first_user
+
+    private
+
+      def assign_to_first_user
+            places.update_all(customer_id: Customer.first.id)
+      end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = Customer.create(email: "admin@admin.com", password: 123456)
+
 c1 = Category.create(name: "Üniversite")
 c2 = Category.create(name: "Restaurant")
 
-Place.create(name: 'Züper Fikir', category: c1)
-Place.create(name: 'Süper Fikir!',  category_id: c1.id)
-Place.create(name: 'Deneme Fikir', category: c2)
+Place.create(name: 'Züper Fikir', category: c1, customer: u)
+Place.create(name: 'Süper Fikir!',  category_id: c1.id, customer: u)
+Place.create(name: 'Deneme Fikir', category: c2, customer: u)
